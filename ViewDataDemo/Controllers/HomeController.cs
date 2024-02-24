2e334d78b5332731443fee03e714f89d65f137ea
dvenkat285 <116060501+dvenkat285@users.noreply.github.com>
2024-02-24 22:08:44 +0530

    Add project files.

---
+++ ViewDataDemo/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ViewDataDemo.Models;
+
+namespace ViewDataDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewData["Message"] = "Message from View Data !!";
+            ViewData["CurrentTime"] = DateTime.Now.ToLongTimeString();
+
+            string[] Fruits = { "Apple", "Banana", "Grapes", "Orange" };
+            ViewData["FruitsArray"] = Fruits;
+
+            ViewData["SportsList"] = new List<string>()
+            {
+                "Cricket",
+                "Hockey",
+                "Football",
+                "Volley Ball"
+            };
+
+            Employee Ali = new Employee();
+            Ali.EmpID = 11;
+            Ali.EmpName = "Ali Khan";
+            Ali.EmpDesignation = "Manager";
+
+            ViewData["Employee"] = Ali;
+
+            return View();
+        }
+    }
+}
